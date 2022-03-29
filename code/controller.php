042e985a0cd1c4ce6229612d9143461aa3f51844
ty-rehsif <tyresef25@hotmail.com>
2022-03-28 19:07:11 -0700

    documentation added, all except R4 completed

--- code/controller.php
+++ code/controller.php
@@ -1,28 +1,29 @@
-<?php session_start();include("templates/page_header.php");?>
+<?php session_start();include("templates/page_header.php");?><!-- starting session and including the page header php -->
 <?php
-if ($_SERVER['REQUEST_METHOD'] === 'POST' && ($_SESSION['token']===$_POST['token'])) {
-    if(!empty($_POST['plant'])){
-    
-    // The request is using the POST method
-    //get table information
-    $control = $_POST['control'];
-    $n_water = $_POST['plant'];
-    if($control ==1){
-        update_status($dbconn, $n_water);
-        }
-    $result = get_plants($dbconn);
-	while ($row = pg_fetch_array($result)) {
-        $data_item['id'] = $row['id'];
-        $data_item['name'] = $row['p_name'];
-        $data_item['last_watered'] = $row['last_watered'];
-        $data_item['status'] = $row['p_status'];
-        $items[] = $data_item;
-        }
-    //send variables to plant monitor
-    $_SESSION['items'] = $items;
-    //echo($items[0]['name']); = plant 1
+//only allow processing when the method is post and when the session csrf token and form sent token are the same
+    if ($_SERVER['REQUEST_METHOD'] === 'POST' && ($_SESSION['token']===$_POST['token'])) {
+        //get the indicator to determine if table should be updated 
+        $control = $_POST['control'];
+        //getting the id of which plant to water 
+        $n_water = $_POST['plant'];
+        //if there is a request to water and the need to water variable is not empty then update
+        if($control ==1 && !(empty($_POST['plant']))){
+            update_status($dbconn, $n_water);
+            }
+        //get the plants from the database
+        $result = get_plants($dbconn);
+        //populate data variable using retrieved/queried database data
+        while ($row = pg_fetch_array($result)) {
+            $data_item['id'] = $row['id'];
+            $data_item['name'] = $row['p_name'];
+            $data_item['last_watered'] = $row['last_watered'];
+            $data_item['status'] = $row['p_status'];
+            $items[] = $data_item;
+            }
+        //send variables to plant monitor
+        $_SESSION['items'] = $items;
+        //echo($items[0]['name']); = plant 1
+        header('Location: plantmonitor.php');
     }
-    header('Location: plantmonitor.php');
-}
 ?>
 <!-- <?php  ?> -->
--- code/plantmonitor.php
+++ code/plantmonitor.php
@@ -1,54 +1,101 @@
 <?php session_start();include("templates/page_header.php");?>
 <!DOCTYPE html>
-<h1>Welcome, thanks for this oppurtunity</h1>
-<h2>Plant Monitor:</h2>
-
-<?php $data = $_SESSION['items'];
-$_SESSION['token'] = (bin2hex(openssl_random_pseudo_bytes(32)));
-$token = $_SESSION['token'];
-?>
-<?php if(empty($data)){ ?>
-<form action="/controller.php" method="POST">
-<input type="hidden" name="csrftoken" value="">
-<input type="submit" value="Start" >
-<?php }?>  
-
-<?php if(!empty($data)){ ?>
-</form>
-<form action="controller.php" method="POST" onsubmit=<?php echo "delay(this);"?>>
-<table>
-    <tr>
-        <th>id</th>
-        <th>name</th>
-        <th>last watered</th>
-        <th>status</th>
-        <th>action</th>
-    </tr>
-        <?php for ($x = 0; $x<sizeof($data); $x++){?>
-    <tr>
-        <td><?php echo $data[$x]['id'];?></td>
-        <td><?php echo $data[$x]['name'];?></td>
-        <?php echo "<script>alert(('$data[$x]['update']'-now())></script>"?>
-        <td><?php echo $data[$x]['last_watered'];?></td>
-        <td><?php echo $data[$x]['status'];?></td>
-        <td>
-            <input type="checkbox" name="plant[]" value="<?=$data[$x]['id']?>">
-            <input type="hidden" name="token" value=<?=$token?>>
-            <input type="hidden" name="control" value="1">
-        </td>
-    </tr>
-        <?php }?>     
-</table>
-<input type="submit" value="submit" onclick="">
-</form>
-<?php } ?>
-<script>
-    function delay(obj) {
-    obj.disabled = true;
-    setTimeout(function() {
-        obj.disabled = false;
-    }, 10000);
+<head>
+<!-- -->
+<!-- css styles-->
+<style>
+    table, th, td{
+        border: 2px solid powderblue;
+        align-content: center;
+        
+    }
+    body{
+    background-color: whitesmoke;
 }
-</script>
-
+h1,h2{
+    text-align: center;
+    font-weight: bold;
+    color:navy;
+    font-family: calibri;
+    text-decoration: underline;
+    text-transform: uppercase;
+}
+.center {
+    margin-left: auto;
+  margin-right: auto;
+}
+</style>
+</head>
+<h1>Welcome, thank you for this oppurtunity</h1>
+<h2>WaterMango:</h2>
+<body>
+<!-- getting items from controller.php -->
+    <?php $data = $_SESSION['items'];
+#creating and setting the csrf token
+    $_SESSION['token'] = (bin2hex(openssl_random_pseudo_bytes(32)));
+#setting the token
+    $token = $_SESSION['token'];
+    ?>
+<!-- if data not populated i.e. start not clicked -->
+    <?php if(empty($data)){ ?>
+        <form action="/controller.php" method="POST">
+            <input type="hidden" name="token" value=<?=$token?>>
+            <input type="submit" value="Start" >
+        </form>
+    <?php }?>
+<!-- if data is in the variable show the table -->
+    <?php if(!empty($data)){ ?>
+        <form action="controller.php" method="POST" name="water_form" id="water_form" onsubmit="water_btn.disabled = true; return true;">
+            <table class = "center" style="width: 60%; text-align: center;">
+                <tr >
+                    <th>ID</th>
+                    <th>Name</th>
+                    <th>Last Time Watered</th>
+                    <th>Current Status</th>
+                    <th>Time Until Next Watering</th>
+                    <th>Select Plant(s) to Water</th>
+                </tr>
+                <!-- populate table using for loop (rows) -->
+                    <?php for ($x = 0; $x<sizeof($data); $x++){?>
+                        <tr>
+                        <!-- plant id -->
+                            <td><?php echo $data[$x]['id'];?></td>
+                        <!-- plant name -->
+                            <td><?php echo $data[$x]['name'];?></td>
+                        <!-- time last watered -->
+                            <td><?php echo date('h:i:s: A', strtotime($data[$x]['last_watered']));?></td>
+                            <td><?php echo $data[$x]['status'];?></td>
+                        <!-- 6 hours after last -->
+                            <td><?php echo $u_time = date('h:i:s: A', (strtotime($data[$x]['last_watered'])+21600));?></td>
+                        <!-- if now = 6 hours after a plant got watered then alert-->
+                            <?php if ($u_time == date('h:i:s: A', strtotime("now"))){echo "<script>alert('Check the plants!')</script>";} ?>
+                        <!-- selecting which plant to water-->
+                            <td>
+                                <input type="checkbox" name="plant[]" value="<?=$data[$x]['id']?>">
+                                <input type="hidden" name="token" value=<?=$token?>>
+                                <input type="hidden" name="control" value="1">
+                            </td>
+                        </tr>
+                    <?php }?>     
+            </table>
+                <!-- wanted to add if coming from a controller.php redirect disable or hide for 30 secs-->
+                <input name="water_btn"  id="water_btn" type="submit" value="Water" onclick=<?php echo "delay(this); alert('Watering...');"?>>
+        </form>
+        <form action = "plantmonitor.php" method="POST"> 
+            <button>Stop Watering</button>
+        </form>
+    <?php } ?>
+</body>
+<script>
+        function delay(obj) {
+        //before click
+        obj.disabled = true;
+        alert("watering!");
+        setTimeout(function() {
+            //after click
+            obj.disabled = false;
+            obj.form.submit();
+            }, 10000);
+        }
+    </script>
 </html>
